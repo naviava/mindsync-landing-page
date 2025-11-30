ceec8ebbbb9daa9353895145b8cade89ca6e5b41
naviava <knavinavadhani@gmail.com>
2025-11-30 10:51:43 +0000

    fix: Fixed the main banner height

--- src/components/main-banner.tsx
+++ src/components/main-banner.tsx
@@ -1,8 +1,11 @@
 export function MainBanner() {
   return (
     <section
-      className="h-[533px] lg:h-[671px] bg-cover bg-center"
-      style={{ backgroundImage: "url('/assets/main-banner.png')" }}
+      className="lg:h-[671px] bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/assets/main-banner.png')",
+        height: `calc(100vh - 121px)`,
+      }}
     >
       <div className="text-white h-full font-bold font-poppins text-[60px] px-10 flex items-center justify-center md:justify-start max-w-[2350px] mx-auto">
         <div>
