2fb6f61ebb335fa0411b0d20a08c65adaa0c25e1
naviava <knavinavadhani@gmail.com>
2025-12-05 12:49:28 +0000

    fix: Fixed banner button alignment

--- src/components/main-banner.tsx
+++ src/components/main-banner.tsx
@@ -29,11 +29,13 @@ export function MainBanner() {
             alt="Underline"
             className="hidden lg:block"
           />
-          <a href="#contact-us">
-            <div className="mt-10 inline-flex items-center justify-center cursor-pointer text-white font-semibold px-10 py-4 text-lg rounded-full border border-cta backdrop-blur-md hover:bg-cta hover:shadow-[0_10px_25px_rgba(0,118,234,0.45)] transition-all duration-300 text-[24px] tracking-wide font-poppins lg:max-w-100">
-              GET IN TOUCH
-            </div>
-          </a>
+          <div className="mx-auto md:mx-0">
+            <a href="#contact-us">
+              <div className="mt-10 inline-flex items-center justify-center cursor-pointer text-white font-semibold px-10 py-4 text-lg rounded-full border border-cta backdrop-blur-md hover:bg-cta hover:shadow-[0_10px_25px_rgba(0,118,234,0.45)] transition-all duration-300 text-[24px] tracking-wide font-poppins lg:max-w-100">
+                GET IN TOUCH
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </section>
